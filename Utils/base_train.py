8e4a58199efe9b64a87057d592ac2db9cf468df6
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-09 12:18:45 -0500

    sorting each batch

--- Utils/base_train.py
+++ Utils/base_train.py
@@ -97,11 +97,17 @@ def sample_train_val_test(ddf, max_samples, time_steps, num_encoder_steps, pred_
                 len(valid_sampling_locations), len(valid_sampling_locations), replace=False)
         ]
 
-    ranges_sublist = [ranges[i:i+batch_size] for i in range(0, len(ranges), batch_size)]
+    ranges = [ranges[i:i+batch_size] for i in range(0, len(ranges), batch_size)]
 
-    ranges_sublist = sorted(ranges_sublist, key=lambda x: x[1], reverse=False)
+    def Sort(List):
+        new_list = []
+        for ls in List:
+            new_list.append(sorted(ls, key=lambda x:x[1]))
+        return new_list
 
-    ranges = reduce(lambda xs, ys: xs + ys, ranges_sublist)
+    ranges = Sort(ranges)
+
+    ranges = reduce(lambda xs, ys: xs + ys, ranges)
 
     input_size = len(enc_input_cols)
     inputs = np.zeros((max_samples, time_steps, input_size))
